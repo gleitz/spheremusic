805f90322e215e954435c47a6a2a457f21e3e50c
Thomas Levine <occurrence@thomaslevine.com>
2013-09-28 18:05:43 -0700

    graphs

--- graph.r
+++ graph.r
@@ -2,5 +2,10 @@ library(ggplot2)
 
 if (!('s' %in% ls())) {
   s <- read.csv('weekly.csv')
-  s$visible <- NULL
+  s$timestamp <- as.Date(s$timestamp)
 }
+p1 <- ggplot(s) + aes(x = longitude, y = latitude) +
+  geom_point()
+p2 <- ggplot(s) + aes(x = velocity) + geom_histogram() + facet_wrap(~ timestamp)
+p3 <- ggplot(s) + aes(x = longitude, y = velocity) + geom_point()
+p4 <- ggplot(s) + aes(y = latitude, x = velocity) + geom_point()
