d96150ff0a8bf0b8466939e605eac70621e3d531
Thomas Levine <occurrence@thomaslevine.com>
2013-09-28 21:34:53 -0700

    add satellite name

--- satellites.py
+++ satellites.py
@@ -55,5 +55,11 @@ def get_location(tle, now=None, lat=None, lng=None):
     lat = degrees(satellite.sublat)
 
     # Return the relevant timestamp and data
-    data = {"timestamp": now.isoformat(), "position": {"latitude": lat, "longitude": lon}, "visible": float(repr(satellite.alt)) > 0 and float(repr(satellite.alt)) < math.pi, "range": satellite.range, "velocity": satellite.range_velocity}
+    data = { "timestamp": now.isoformat(),
+             "satellite": satellite.name,
+             "latitude": lat,
+             "longitude": lon,
+             "visible": float(repr(satellite.alt)) > 0 and float(repr(satellite.alt)) < math.pi,
+             "range": satellite.range,
+             "velocity": satellite.range_velocity }
     return data
