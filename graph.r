5e2396e21821bdc23687711f7eec927c82212503
Thomas Levine <occurrence@thomaslevine.com>
2013-09-28 21:42:32 -0700

    aoeu

--- graph.r
+++ graph.r
@@ -9,3 +9,7 @@ p1 <- ggplot(s) + aes(x = longitude, y = latitude) +
 p2 <- ggplot(s) + aes(x = velocity) + geom_histogram() + facet_wrap(~ timestamp)
 p3 <- ggplot(s) + aes(x = longitude, y = velocity) + geom_point()
 p4 <- ggplot(s) + aes(y = latitude, x = velocity) + geom_point()
+
+p5 <- ggplot(s) + coord_map(projection = 'mercator') +
+  aes(x = longitude, y = latitude, group = satellite) +
+  geom_path()
